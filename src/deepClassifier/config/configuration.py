58aeb80e3a08bebeaa5bc6de622236120a6f69fe
rivillasm <rivillasm@gmail.com>
2022-09-28 00:22:30 -0600

    updated

--- src/deepClassifier/config/configuration.py
+++ src/deepClassifier/config/configuration.py
@@ -5,8 +5,7 @@ from pathlib import Path
 
 class ConfigurationManager:
     def __init__(
-        self, config_filepath=CONFIG_FILE_PATH, params_filepath=PARAMS_FILE_PATH
-    ):
+        self, config_filepath=CONFIG_FILE_PATH, params_filepath=PARAMS_FILE_PATH):
         self.config = read_yaml(config_filepath)
         self.params = read_yaml(params_filepath)
         create_directories([self.config.artifacts_root])
